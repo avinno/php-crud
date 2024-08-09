47e26016a3f37979e33333d8245688a99eceac61
Aaron Varga <aaronvarga@onmail.com>
2024-08-09 15:13:47 -0400

    gitignore fix

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>PHP Crud App by Aaron Varga</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <?php $name = "Aaron"; ?>
+        <?php echo "<h1>Welcome back, {$name}!</h1>"; ?>
+    </header>
+    <main></main>
+    <footer></footer>
+    <script src="script.js" defer></script>
+</body>
+</html>
